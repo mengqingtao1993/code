1a3350be4e675eaaadee604826b51269aeb4f169
mengqingtao <mengqingtao@51shebao.com>
2022-02-09 19:11:33 +0800

    cv

--- 1.promise.js
+++ 1.promise.js
@@ -1,1 +1,24 @@
-class Promise{}
+class Promise {
+
+}
+
+
+// deferred 返回一个延迟对象
+Promise.deferred = function () {
+  let deferred = {}
+  deferred.promise = new Promise((resolve, reject) => {
+    deferred.resolve = resolve
+    deferred.reject = reject
+  })
+  return deferred
+}
+
+// catch
+Promise.prototype.catch = function (err) {
+  return this.then(null, err)
+}
+
+// finally 无论如何都会执行它,然后继续往后抛状态
+Promise.prototype.finally = function(){
+  
+}
---
+++ 2.reduce.js
@@ -0,0 +1,18 @@
+// 实现reduce
+Function.prototype.reduce = function(){
+
+}
+
+
+
+// 实现map
+function myMap(){
+
+}
+
+
+
+// 实现compose
+function compose(){
+  
+}
